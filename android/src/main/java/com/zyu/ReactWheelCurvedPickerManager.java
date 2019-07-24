25b48172fb09805ceb476b71213e4557e248f374
Santosh Patidar <spatidar@dingg.app>
2019-07-24 16:15:25 +0530

    Fix to show selected item

--- android/src/main/java/com/zyu/ReactWheelCurvedPickerManager.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPickerManager.java
@@ -21,14 +21,14 @@ public class ReactWheelCurvedPickerManager extends SimpleViewManager<ReactWheelC
 
     private static final String REACT_CLASS = "WheelCurvedPicker";
 
-    private static final int DEFAULT_TEXT_SIZE = 25 * 2;
+    private static final int DEFAULT_TEXT_SIZE = 25;
     private static final int DEFAULT_ITEM_SPACE = 14 * 2;
 
     @Override
     protected ReactWheelCurvedPicker createViewInstance(ThemedReactContext reactContext) {
         ReactWheelCurvedPicker picker = new ReactWheelCurvedPicker(reactContext);
         picker.setTextColor(Color.LTGRAY);
-        picker.setCurrentTextColor(Color.WHITE);
+        picker.setCurrentTextColor(Color.BLACK);
         picker.setTextSize(DEFAULT_TEXT_SIZE);
         picker.setItemSpace(DEFAULT_ITEM_SPACE);
 
